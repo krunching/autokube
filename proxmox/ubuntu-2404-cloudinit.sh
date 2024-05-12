d5b87d9c33a40206c27c530a8c4a2a64b98df57b
krunching <62070280+krunching@users.noreply.github.com>
2024-05-12 17:14:48 +0200

    fix permission

--- proxmox/ubuntu-2404-cloudinit.sh
+++ proxmox/ubuntu-2404-cloudinit.sh
@@ -38,7 +38,6 @@ qm template 9000
 # Script derived from https://github.com/UntouchedWagons/Ubuntu-CloudInit-Docs/blob/main/samples/ubuntu/ubuntu-noble-cloudinit.sh
 
 # Prepare terraform role and user
-# Edit <password>
 pveum role add terraformProv -privs "VM.Allocate VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.Audit VM.PowerMgmt Datastore.AllocateSpace Datastore.Audit"
 pveum user add terraform-prov@pve --password $TF_USERPASS
 pveum aclmod / -user terraform-prov@pve -role terraformProv
